bfa84881f86205ed286f01dd4d42f78d68c2417f
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-03-23 18:18:29 +0100

    Modification du fichier ArticlesFixtures.php pour mettre en place un jeu de données, les faires persister et les envoyer dans la bdd

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -4,14 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <=10; $i++)
+        {
+            $article = new Article();
+            $article    ->setTitle("Titre de l'article n°$i")
+                        ->setContent("<p>Contenu de l'article n°$i</p>")
+                        ->setImage("http://placehold.it/350x150")
+                        ->setCreateAt(new \DateTime());
 
-        $manager->flush();
+            $manager->persist($article); //Fonction qui prépare la persistance de l'article.
+        }
+        $manager->flush(); //Fonction qui balance réellement les requêtes SQL pour la création dan la bdd. 
     }
 }
