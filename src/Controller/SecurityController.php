91bd7df19bbb8d39cc50a0c4d29d5de777d086e7
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-03-27 11:58:10 +0100

    Modification du fichier SecurityController.php. On supprime la classe par défaut index() et on met la function registration().

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegistrationType;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/inscription", name="security_registration")
      */
-    public function index()
+    public function registration()
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
