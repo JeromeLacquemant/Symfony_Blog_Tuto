5bbac9475727710073daad613344014e9a13b1db
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-03-27 14:28:26 +0100

    Mise en relation du formulaire et de la base de données (modification de la fonction registration() de SecurityController.php)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,20 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration()
+    public function registration(Request $request, ManagerRegistry $manager)
     {
         $user = new User();
+
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->getManager()->persist($user);
+            $manager->getManager()->flush();
+        }
 
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
